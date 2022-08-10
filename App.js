bd3c1377a07a2f9f331924568b642b1ba546aa1e
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-08-10 11:32:19 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+
+function HomeScreen({navigation,route}) {
+  React.useEffect(()=>){}
+}
+
+function CreatePostScreen() {
+
+    
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#008b8b" },
+          headerTintColor: "#ffff",
+          headerTitleStyle: { fontWeight: "bold", fontSize: 30 },
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={CreatePostScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screen/HomeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text, Button, TextInput } from 'react-native'
+import React from 'react'
+
+import { Ionicons } from '@expo/vector-icons'
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Ionicons name='name' size={30} color='#008b8b'/>
+      <Text>Home Screen</Text>
+      <Button 
+        title='เกี่ยวกับเรา'
+        onPress={()=>navigation.navigate('About',{
+            email:'reactnative.tni.ac.th'
+        })}
+       />
+    </View>
+  )
+}
+
+export default HomeScreen
