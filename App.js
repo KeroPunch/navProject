9e3cb37442275ce5fa95b3468c9c71ed999c329d
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-08-10 11:40:41 +0700

    Commit 10/8/2022

--- App.js
+++ App.js
@@ -1,17 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, Button, TextInput } from 'react-native';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 
-function HomeScreen({navigation,route}) {
-  React.useEffect(()=>){}
+
+function HomeScreen({ navigation, route }) {
+
+  React.useEffect(() => {
+    if (route.params?.post) {
+      //Post updated,do somethin with 'route.params.post'
+      //For example, sent the post to the server      
+    }
+  }, [route.params?.post]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title='Create Post'
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post:{route.params?.post}</Text>
+    </View>
+  );
 }
 
-function CreatePostScreen() {
+function CreatePostScreen({ navigation, route }) {
 
-    
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    // use Fragment
+    <>
+      <TextInput
+        multiline
+        placeholder='Plese text here'
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        onChangeText={setPostText}
+        value={postText}
+      />
+      <Button
+        title='Click'
+        onPress={() => {
+          //Pass params back to HomeScreen funchion
+          navigation.navigate('Home', { post: postText })
+        }}
+      />
+    </>
+
+  );
 }
 
 const Stack = createNativeStackNavigator();
@@ -20,16 +58,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName='Home'
         screenOptions={{
-          headerStyle: { backgroundColor: "#008b8b" },
-          headerTintColor: "#ffff",
-          headerTitleStyle: { fontWeight: "bold", fontSize: 30 },
+          headerStyle: { backgroundColor: 'blue' },
+          headerTintColor: '#ffff',
+          headerTitleStyle: { fontWeight: 'bold' }
         }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={CreatePostScreen} />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='CreatePost' component={CreatePostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+
   );
 }
