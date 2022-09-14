e576928a7b4aeaffa1a74d45fa1e094ec5153c3b
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-09-14 10:16:00 +0700

    commit 14/9/65

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import React from "react";
-import { NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+    background: "rgb(250, 255, 255 )",
+  },
+};
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+
+      <Drawer.Navigator useLegacyImplementation>
+        <Drawer.Screen name='Home'component={MyTap}/>
+        <Drawer.Screen name="Setting" component={SettingScreen}/>
+      </Drawer.Navigator>
+
+  )
+}
 
 function HomeScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-      <Button title="Go to Setting" onPress={()=>navigation.navigate("Setting")}/>
+  return(
+    <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Ionicons name="home" size={30} color="#42AC42" />
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
 function SettingScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Setting!</Text>
-      <Button title="Go to Home" onPress={()=>navigation.navigate("Home")}/>
+  return(
+    <View style = {{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>Setting Screen</Text>
+      <Button title="Go to Home"
+      onPress={() => navigation.goBack()}/>
     </View>
   );
 }
@@ -24,7 +55,21 @@ const Tab = createBottomTabNavigator();
 
 function MyTap() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator  screenOptions={({ route }) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        if(route.name ==='Home') {
+          iconName = focused
+          ? 'ios-information-circle'
+          : 'ios-information-circle-outline'
+        }else if (route.name === 'Setting'){
+          iconName = focused ? 'ios-list-box' : 'ios-list'
+        }
+        return <Ionicons name={iconName} size={size} color={color}/>
+      },
+      tabBarActiveTintColor:'tomato',
+      tabBarInactiveTintColor:'gray'
+    })}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Setting" component={SettingScreen} />
     </Tab.Navigator>
@@ -33,8 +78,8 @@ function MyTap() {
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MyTap />
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer/>
     </NavigationContainer>
   );
 };
